51209a3be1a1d506673affbaf5d0376bb29f5bbe
Shreya <www.shreyaagarwal.com@gmail.com>
2022-07-04 00:04:59 +0530

    Stacks added

---
+++ 06.Stacks/105.DuplicateBrackets.cpp
@@ -0,0 +1,21 @@
+/*
+
+	(a + b) + (( c + d )) --> true ( yes, duplicacy is there)
+	((a + b) + (c + d )) ---> false (no, no duplicacy)  
+
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+
+	string s;
+	cin>>s;
+	stack<char> st;
+
+	
+
+
+	return 0;
+}
