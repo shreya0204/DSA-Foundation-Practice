8572536c88dd1ed69f5ddb3b2e96320e91bc9344
Shreya <www.shreyaagarwal.com@gmail.com>
2022-07-04 00:04:09 +0530

    Diff of two arrays question added

--- 05.Arrays/102.DiffTwoArrays.cpp
+++ 05.Arrays/102.DiffTwoArrays.cpp
@@ -21,7 +21,44 @@ int main(){
 
 	// Logic
 
-	
+
+	int diff[n2];
+	int c= 0;
+	int i=n1-1;
+	int j= n2-1;
+	int k = n2 - 1;
+
+
+	while(k>=0){
+		int d = 0;
+		int val = i>=0? arr1[i] : 0;
+		if(arr2[j] + c  >= arr1[i]){
+			d = arr2[j] + c - val;
+			c = 0;
+		}else{
+			d = arr2[j] + c + 10 - val;
+			c = -1;
+		}
+
+		diff[k] = d;
+
+		i--;
+		j--;
+		k--;
+	} 
+
+	int index = 0;
+	while(index < n2 - 1){
+		if(diff[index] == 0){
+			index++;
+		}else{
+			break;
+		}
+	}
+
+	while(index < n2 - 1){
+		cout<<diff[index++]<<" ";
+	}
 
 	return 0;
 }
